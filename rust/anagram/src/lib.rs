5b4a10a118ba3d0879f313d1aa22009a8543e8ba
Vicente <vicentedpsantos@gmail.com>
2021-04-23 17:18:59 -0300

    Add anagram exercise

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,49 @@
+use std::collections::HashSet;
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let target_word = TargetWord {
+        value: &word.to_lowercase(),
+    };
+
+    let mut outputs: Vec<&str> = vec![];
+
+    for word in possible_anagrams {
+        if target_word.test(&word.to_lowercase()) {
+            outputs.push(word);
+        }
+    }
+
+    outputs.iter().cloned().collect()
+}
+
+struct TargetWord<'a> {
+    value: &'a str,
+}
+
+impl<'a> TargetWord<'a> {
+    fn test(&self, word: &str) -> bool {
+        if self.is_different(&word) && self.is_same_length(&word) && self.is_anagram(&word) {
+            return true;
+        }
+
+        false
+    }
+
+    fn is_different(&self, word: &str) -> bool {
+        !self.value.eq_ignore_ascii_case(word)
+    }
+
+    fn is_same_length(&self, word: &str) -> bool {
+        self.value.len() == word.len()
+    }
+
+    fn is_anagram(&self, word: &str) -> bool {
+        let mut self_chars: Vec<char> = self.value.chars().collect();
+        self_chars.sort_by(|a, b| b.cmp(a));
+
+        let mut incoming_chars: Vec<char> = word.chars().collect();
+        incoming_chars.sort_by(|a, b| b.cmp(a));
+
+        self_chars == incoming_chars
+    }
+}
