ee023e4cf7ac5ed02341bb81f809d8efede14b14
Vicente <vicentedpsantos@gmail.com>
2021-04-26 16:48:38 -0300

    Add macros to space age exercise

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -22,77 +22,32 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn base() -> bool {
-        false
-    }
+    const ORBITAL_PERIOD: f64;
+    fn years_during(d: &Duration) -> f64;
+}
 
-    fn orbital_period() -> f64 {
-        unimplemented!()
-    }
+macro_rules! planet {
+    ($name:ident, $period:expr) => {
+        pub struct $name;
+        impl Planet for $name {
+            const ORBITAL_PERIOD: f64 = $period;
 
-    fn years_during(d: &Duration) -> f64 {
-        let base_result: f64 = d.seconds / 60. / 60. / 24. / Earth::orbital_period();
-
-        match Self::base() {
-            true => base_result,
-            _ => base_result / Self::orbital_period(),
+            fn years_during(d: &Duration) -> f64 {
+                d.seconds / Self::ORBITAL_PERIOD / (Earth::YEAR_SECONDS as f64)
+            }
         }
-    }
+    };
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Earth, 1.0);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
 
-impl Planet for Mercury {
-    fn orbital_period() -> f64 {
-        0.2408467
-    }
-}
-impl Planet for Venus {
-    fn orbital_period() -> f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn base() -> bool {
-        true
-    }
-
-    fn orbital_period() -> f64 {
-        365.25
-    }
-}
-
-impl Planet for Mars {
-    fn orbital_period() -> f64 {
-        1.8808158
-    }
-}
-
-impl Planet for Jupiter {
-    fn orbital_period() -> f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn orbital_period() -> f64 {
-        29.447498
-    }
-}
-
-impl Planet for Uranus {
-    fn orbital_period() -> f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn orbital_period() -> f64 {
-        164.79132
-    }
+impl Earth {
+    pub const YEAR_SECONDS: u64 = 31557600;
 }
