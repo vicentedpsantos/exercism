4018fcc78ce9cd1f03ea71ade2915cfd5c1c36f8
Vicente <vicentedpsantos@gmail.com>
2021-05-03 16:05:53 -0300

    Fix

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -12,7 +12,7 @@ pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison 
         (0, _) => Comparison::Sublist,
         (_, 0) => Comparison::Superlist,
         (f, s) if f < s && is_sublist(first_list, second_list) => Comparison::Sublist,
-        (s, f) if s < f && is_sublist(second_list, first_list) => Comparison::Superlist,
+        (s, f) if s > f && is_sublist(second_list, first_list) => Comparison::Superlist,
         (s, f) if s == f && first_list == second_list => Comparison::Equal,
         _ => Comparison::Unequal,
     }
