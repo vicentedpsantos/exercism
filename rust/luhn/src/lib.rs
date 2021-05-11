570127e0646073844be0c013e4b0725f1344eb1c
Vicente <vicentedpsantos@gmail.com>
2021-05-11 10:40:00 -0300

    Add more performant luhn solution

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,68 +1,25 @@
-use regex::Regex;
-
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    let code = CardString::new(code);
+    let mut sum = 0;
+    let len = code.trim().len();
 
-    code.is_valid()
-}
+    const RADIX: u32 = 10;
 
-struct CardString {
-    value: String,
-    _secret: (),
-}
+    for (i, c) in code.chars().rev().filter(|&c| c != ' ').enumerate() {
+        match (i % 2, c.to_digit(RADIX)) {
+            (1, Some(x)) => {
+                let mut temp = x * 2;
 
-impl CardString {
-    fn new(code: &str) -> Self {
-        let whitespacesre = Regex::new(r"\s").unwrap();
-        let value = whitespacesre.replace_all(code, "");
-
-        Self {
-            value: value.to_string(),
-            _secret: (),
-        }
-    }
-
-    fn is_valid(&self) -> bool {
-        let non_digitre = Regex::new(r"\D").unwrap();
-        let non_digits = match non_digitre.find(&self.value) {
-            Some(x) => x.as_str(),
-            None => "",
-        };
-
-        self.value.len() > 1 && non_digits.len() == 0 && self.is_luhn()
-    }
-
-    fn is_luhn(&self) -> bool {
-        (self.luhn_sum() % 10) == 0
-    }
-
-    fn luhn_sum(&self) -> i32 {
-        let intgs: Vec<i32> = self
-            .value
-            .chars()
-            .rev()
-            .map(|x| x.to_string().parse::<i32>().unwrap())
-            .collect();
-
-        let mut res_list: Vec<i32> = Vec::new();
-
-        for (i, v) in intgs.iter().enumerate() {
-            let mut res: i32 = *v;
-
-            if i % 2 != 0 {
-                res = 2 * v;
-
-                if res > 9 {
-                    res = res - 9
+                if temp > 9 {
+                    temp -= 9;
                 }
+
+                sum += temp;
             }
-
-            res_list.push(res)
+            (0, Some(x)) => sum += x,
+            (_, _) => return false,
         }
-
-        let result = res_list.iter().sum();
-
-        result
     }
+
+    (len > 1) && (sum % 10 == 0)
 }
