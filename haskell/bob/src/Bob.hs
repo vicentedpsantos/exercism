ae7aa854f2c0113b22a3053a0ddf25bfc3b9c889
Vicente Santos <vicentedpsantos@gmail.com>
2022-09-13 03:02:44 +0100

    Solve problem with importing Data.Map and Data.Char

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,8 +1,5 @@
 module Bob (responseFor) where
 
-import qualified Data.Map  as Map
-import qualified Data.Char as Char
-
 data Interaction = Question
                  | Shout
                  | ShoutedQuestion
@@ -10,36 +7,55 @@ data Interaction = Question
                  | Default
     deriving(Ord, Eq, Show)
 
-type ResponseMap = Map.Map Interaction String
+type Answer = String
+type ResponseMap = [(Interaction, Answer)]
 
 responses :: ResponseMap
-responses = Map.fromList
-            [ (Question, "Sure.")
+responses = [ (Question, "Sure.")
             , (Shout, "Whoa, chill out!")
-            , (ShoutedQuestion, "Calm down, I know what I'm doing")
+            , (ShoutedQuestion, "Calm down, I know what I'm doing!")
             , (Silence, "Fine. Be that way!")
             , (Default, "Whatever.")
             ]
 
 responseFor :: String -> String
 responseFor message =
-  case Map.lookup (fetchInteraction message) responses of
+  case findKey (fetchInteraction message) responses of
       Nothing -> "Whatever."
       Just response -> response
   
 
 fetchInteraction :: String -> Interaction
 fetchInteraction message
-    | null message                          = Silence
+    | isSilence message                     = Silence
     | isQuestion message && isShout message = ShoutedQuestion
     | isShout message                       = Shout
     | isQuestion message                    = Question
     | otherwise                             = Default
 
+findKey :: (Eq a) => a -> [(a, b)] -> Maybe b
+findKey key = foldr(\(k, v) acc -> if k == key then Just v else acc) Nothing
+
 isShout :: String -> Bool
 isShout message =
-    let uppercasedMessage = map Char.toUpper message
-    in uppercasedMessage == message
+    let uppercasedMessage = uppercase message == message
+        notOnlyNumbers = not $ onlyNumbers message
+        anyText = any (`elem` ['A'..'Z']) message
+    in uppercasedMessage && notOnlyNumbers && anyText
+
+onlyNumbers :: String -> Bool
+onlyNumbers = all (`elem` ['0'..'9'] ++ [' ', ',', '?'])
 
 isQuestion :: String -> Bool
-isQuestion xs = last xs == '?'
+isQuestion xs =
+    let lastCharacter = last . last $ words xs
+    in lastCharacter == '?'
+
+uppercase :: String -> String
+uppercase = map (\c -> if c >= 'a' && c <= 'z' then toEnum (fromEnum c - 32) else c)
+
+whiteSpaces :: [Char]
+whiteSpaces = [' ', '\t', '\n', '\r']
+
+isSilence :: String -> Bool
+isSilence = foldr (\x acc -> if not $ x `elem` whiteSpaces then False else acc) True
